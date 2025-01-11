98fbd1ff7171074ad5e00a9d890cbda14a4fe3eb
Dhruv Trivedi <dhrtr.36@gmail.com>
2025-01-11 18:49:25 +0530

    publish fuzzrmirsi

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("fuzzrmirsi has started executing!");
+    
+    // Your fuzzing logic here
+    println!("Fuzzing process running...");
+    
+    // Example for checking if the application hits a certain point
+    let result = 4;
+    println!("Fuzzing result: {:?}", result);
+}
